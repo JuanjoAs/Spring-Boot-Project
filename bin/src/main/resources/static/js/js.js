4f659dd1d1342d580de6f0b0ba70628acd4eec2e
JuanjoAs <43440655+JuanjoAs@users.noreply.github.com>
2020-04-09 00:15:12 +0200

    prueba

---
+++ bin/src/main/resources/static/js/js.js
@@ -0,0 +1,27 @@
+
+
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left'); 
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    SidebarCollapse();
+});
+
+function SidebarCollapse () {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+    
+    
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+    
+    
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
